c3559d1466c9eeaef5651f6073278be1cce7c1a4
AhmedHesham0100 <ahmedhesham0100@gmail.com>
2022-04-27 20:48:18 +0200

    add loader

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import axios from 'axios';
 
 import Card from './component/Card';
 
+import Loader from './component/Loader';
+
 function App() {
 
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
 
@@ -23,36 +25,37 @@ function App() {
       .catch((err) => console.error(err));
   }
 
-
   useEffect(() => {
 
     getProducts();
   }, [])
 
   return (
-    <Fragment >
+    <Fragment>
 
       <header className='flex items-center justify-center font-serif text-2xl font-bold bg-gray-400 rounded-2xl p-5'>
 
         products list
       </header>
 
-      <div className='container mx-auto my-5'>
+      {products && products.length > 0 ?
 
-        <div className='grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 gap-4'>
+        <div className='container mx-auto my-5'>
 
-          {products && products.map(product => (
+          <div className='grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 gap-4'>
 
-            <div key={product.id}>
+            {products.map(product => (
 
-              <Card product={product} />
+              <Card product={product} key={product.id} />
 
-            </div>
+            ))}
 
-          ))}
+          </div>
 
         </div>
-      </div>
+
+        : <Loader />
+      }
 
     </Fragment>
   );
